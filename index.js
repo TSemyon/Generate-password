4f08fa7f8ba4a7777a9e3994c7731f0d6f12ced4
Semyon <semen@MBP-Semen.Dlink>
2022-09-07 10:40:38 +0300

    A small password generator from four to sixteen characters.

---
+++ index.js
@@ -0,0 +1,49 @@
+window.addEventListener('DOMContentLoaded', () => {
+  const btnWelcome = document.querySelector('.welcome__button'),
+        btnChoice = document.querySelectorAll('.choice__button'),
+        welcomeSection = document.querySelector('.welcome'),
+        choiceSection = document.querySelector('.choice'),
+        generateSection = document.querySelector('.generate')
+        btnCopy = document.querySelector('#copy'),
+        btnGenerate = document.querySelector('#generate'),
+        input = document.querySelector('.generate__input')
+
+  btnWelcome.addEventListener('click', () => {
+      welcomeSection.classList.toggle('active')
+      choiceSection.classList.toggle('active')
+  })
+
+   btnChoice.forEach(btn => {
+      btn.addEventListener('click', (e) => {
+         choiceSection.classList.toggle('active')
+         generateSection.classList.toggle('active')
+         let number = +e.target.innerText
+
+         const symbols = "0123456789ABCDEFGHIefghijklmnopqrJKLMNOPQR!@#$%&*%:,;_STUVWXYZ0123456789abcdstuvwxyz!$;%:?*#_"
+         const numbers = "0123456789"
+
+         function generatePassword(num,symbol) {
+            let randomPassword = "";
+            for (let i = 0; i < num; i++){
+               let random = Math.floor(Math.random() * symbol.length); 
+               randomPassword += symbol[random]
+            }
+            return randomPassword
+         }
+         
+         if(number > 4) {
+            btnGenerate.addEventListener('click', () => {
+               input.value =  generatePassword(number, symbols)
+            })
+         } else {
+            btnGenerate.addEventListener('click', () => {
+               input.value =  generatePassword(number, numbers)
+            })
+         }
+      })
+   })
+})
+
+
+
+
